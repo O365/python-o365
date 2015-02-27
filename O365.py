31c0c7451442be2d47fd40dc2b95231a012d10b2
Toben Archer <sandslash@gmail.com>
2015-02-27 15:15:25 +0000

    Praise God from whom all blessings flow.

--- O365.py
+++ O365.py
@@ -1,7 +1,2 @@
-from O365Attachment import *
-from O365Inbox import *
-from O365Message import *
-from O365Calendar import *
-from O365Event import *
-from O365 import *
-from O365Schedule import *
+
+#To the King!
--- emailprinting.py
+++ emailprinting.py
@@ -1,24 +1,2 @@
-from O365 import *
-from printing import *
-import json
 
-if __name__ == '__main__':
-	with open('./ep.pw','r') as configFile:
-		config = configFile.read()
-		cjson = json.loads(config)
-
-	e = cjson ['email']
-	p = cjson ['password']
-
-	i = Inbox(e,p)
-	i.getMessages()
-
-        printer = getRicoh()
-
-	for m in i.messages:
-		m.fetchAttachments()
-		for att in m.attachments:
-			p = att.byteString()
-			if p:
-				ret = printer.sendPrint(p)
-				print ret
+#To the King!
