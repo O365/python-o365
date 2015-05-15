e98db42f6c0dd6bdbea49f2af9aa82d254775107
Toben Archer <sandslash@gmail.com>
2015-05-15 17:40:54 +0100

    Got contact to have it's most important features. Still needs some polish, but it is functional. I'll write tests and such later. now it is the weekend and I intend to go watch some voyager.

--- O365/contact.py
+++ O365/contact.py
@@ -50,6 +50,9 @@ class Contact( object ):
 			log.debug('translating contact information into local variables.')
 			self.contactId = json['Id']
 			self.name = json['DisplayName']
+		else:
+			log.debug('there was no json, putting in some dumby info.')
+			self.json = {'DisplayName':
 
 	def delete(self):
 		'''delete's a contact. cause who needs that guy anyway?'''
@@ -85,11 +88,29 @@ class Contact( object ):
 		return Contact(response.json(),self.auth)
 
 	def create(self):
-		pass
+		'''create a contact with information in the local json.'''
+		if not self.auth:
+			log.debug('no authentication information, cannot create')
+			return false
 
-	def getName(self):
-		'''Get the contact's Name.'''
-		return self.json['DisplayName']
+		headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+		data = json.dumps(self.json)
+
+		response = None
+		try:
+			response = requests.post(self.con_url.format(str(self.contactId)),auth=self.auth)
+			log.debug('sent create request')
+		except Exception as e:
+			if response:
+				log.debug('response to contact create: {0}'.format(str(response)))
+			else:
+				log.error('No response, something is very wrong with create: {0}'.format(str(e)))
+			return False
+
+		log.debug('Response to contact create: {0}'.format(str(response)))
+
+		return Contact(response.json(),self.auth)
 
 	def getContactId(self):
 		'''Get contact's GUID for office 365. mostly used interally in this library.'''
@@ -98,6 +119,14 @@ class Contact( object ):
 	def getId(self):
 		'''Get contact's GUID for office 365. mostly used interally in this library.'''
 		return self.getContactId()
+
+	def getName(self):
+		'''Get the contact's Name.'''
+		return self.json['DisplayName']
+
+	def setName(self,val):
+		'''sets the display name of the contact.'''
+		self.json['DisplayName'] = val.
 
 	def getFirstEmailAddress(self):
 		'''Get the contact's first Email address. returns just the email address.'''
@@ -109,6 +138,22 @@ class Contact( object ):
 		for e in self.json['EmailAddresses']:
 			ret.append(e['Address'])
 
+	def getEmailAddress(self,loc):
+		'''
+		This method will return the email address, text only, from the specified location.
+		As the order in which the addresses may have downloaded is non-deterministic, it can
+		not be garunteed that the nth address will be in the same position each time.
+		'''
+		return self.json['EmailAddresses'][loc]['Address']
+
+	def setEmailAddress(self,val,loc):
+		'''
+		Sets the email address of the specified index. The download of this information may
+		not be the same each time, so besure you know which address you are editing before 
+		you use this method.
+		'''
+		self.json['EmailAddress'][loc]['Address']
+
 	def getFirstEmailInfo(self):
 		'''gets an email address and it's associated date for the first email address.'''
 		return self.json['EmailAddresses'][0]
@@ -116,5 +161,18 @@ class Contact( object ):
 	def getAllEmailInfo(self):
 		'''Gets email addresses and any data that goes with it such as name, returns dict'''
 		return self.json['EmaillAddresses']
+
+	def setEmailInfo(self,val):
+		'''set the list of email addresses. Must be formated as such:
+		[{"Address":"youremail@example.com","Name","your name"},{and the next]
+		this replaces current inplace email address information.
+		'''
+		self.json['EmailAddresses'] = val
+
+	def addEmail(self,address,name=None):
+		'''takes a plain string email, and optionally name, and appends it to list.'''
+		ins = {'Address':address,'Name':None
+
+	
 
 #To the King!
