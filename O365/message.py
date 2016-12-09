d02c6ba729a4ca97178126cd7ebf94ba390db45d
ajwillo <alexwilloughby@outlook.com>
2016-12-09 18:01:21 +0000

    Update message.py
    Added getDateTime function

--- O365/message.py
+++ O365/message.py
@@ -143,6 +143,10 @@ class Message( object ):
 	def getSubject(self):
 		'''get email subject line.'''
 		return self.json['Subject']
+	
+	def getDateTime(self):
+		'''get email date.'''
+		return self.json['DateTimeReceived']     
 
 	def getBody(self):
 		'''get email body.'''
