c41432867a8c652059b7bc3fec23eb261f1c2875
Royce Melborn <royce@outlook.in>
2018-09-26 03:57:18 +0530

    Enhancements and Bugfixes
    fluent_message moves more towards fluency

--- O365/fluent_message.py
+++ O365/fluent_message.py
@@ -1,14 +1,18 @@
+from O365 import Connection
 from O365.attachment import Attachment
+from O365.connection import MicroDict
 from O365.contact import Contact
 from O365.group import Group
 import logging
 import json
 import requests
 
+from O365.utils import deprecated
+
 log = logging.getLogger(__name__)
 
 
-class Message(object):
+class FluentMessage(object):
     '''
     Management of the process of sending, receiving, reading, and editing emails.
 
@@ -39,86 +43,81 @@ class Message(object):
 
     '''
 
-    att_url = 'https://outlook.office365.com/api/v1.0/me/messages/{0}/attachments'
-    send_url = 'https://outlook.office365.com/api/v1.0/me/sendmail'
-    send_as_url = 'https://outlook.office365.com/api/v1.0/users/{user_id}/sendmail'
-    draft_url = 'https://outlook.office365.com/api/v1.0/me/folders/{folder_id}/messages'
-    update_url = 'https://outlook.office365.com/api/v1.0/me/messages/{0}'
+    url_dict = {
+        'attachments': '/me/messages/{message_id}/attachments',
+        'send': '/me/sendmail',
+        'send_as': '/me/users/{user_id}/sendmail',
+        'draft': '/me/folders/{folder_id}/messages',
+        'update': '/me/messages/{message_id}',
+        'move': '/me/messages/{0}/move',
+    }
 
-    def __init__(self, json=None, auth=None, verify=True, oauth=None):
-        '''
-        Makes a new message wrapper for sending and receiving messages.
+    @staticmethod
+    def _get_url(key):
+        """ Fetches the url for specified key as per the connection version
+        configured
 
-        Keyword Arguments:
-                        json (default = None) -- Takes json if you have a pre-existing message to create from.
-                        this is mostly used inside the library for when new messages are downloaded.
-                        auth (default = None) -- Takes an (email,password) tuple that will be used for
-                        authentication with office365.
-        '''
-        if json:
-            self.json = json
-            self.hasAttachments = json['HasAttachments']
+        :param key: the key for which url is required
+        :return: URL to use for requests
+        """
+        return Connection().root_url + FluentMessage.url_dict[key]
+
+    def __init__(self, json_data=None, verify=True):
+        """ Makes a new message wrapper for sending and receiving messages.
+
+        :param json_data: Takes json if you have a pre-existing message to
+         create from. this is mostly used inside the library for
+         when new messages are downloaded.
+        :param verify: whether or not to verify SSL certificate
+
+        """
+        if json_data:
+            self.json = json_data
+            self.has_attachments = json_data['hasAttachments']
 
         else:
-            self.json = {'Message': {'Body': {}},
-                         'ToRecipients': [], 'CcRecipients': [],
-                         'BccRecipients': []}
-            self.hasAttachments = False
+            self.json = MicroDict({'Message': {'Body': {}},
+                                   'ToRecipients': [], 'CcRecipients': [],
+                                   'BccRecipients': []})
+            self.has_attachments = False
 
-        self.auth = auth
+        # Added for backward compatibility
+        self.hasAttachments = self.has_attachments
+
         self.attachments = []
         self.receiver = None
         self.verify = verify
-        self.oauth = oauth
 
-        # Update to 2.0 versions
-        if self.oauth:
-            self.att_url = self.att_url.replace(
-                'https://outlook.office365.com/api',
-                'https://graph.microsoft.com')
-            self.send_url = self.send_url.replace(
-                'https://outlook.office365.com/api',
-                'https://graph.microsoft.com')
-            self.send_as_url = self.send_as_url.replace(
-                'https://outlook.office365.com/api',
-                'https://graph.microsoft.com')
-            self.draft_url = self.draft_url.replace(
-                'https://outlook.office365.com/api',
-                'https://graph.microsoft.com').replace(
-                'folders', 'MailFolders', 1)
-            self.update_url = self.update_url.replace(
-                'https://outlook.office365.com/api',
-                'https://graph.microsoft.com')
+    def fetch_attachments(self, **kwargs):
+        """ Downloads the attachments
 
-    def fetchAttachments(self, **kwargs):
-        '''kicks off the process that downloads attachments locally.'''
-        if not self.hasAttachments:
-            log.debug('message has no attachments, skipping out early.')
+        :return no. of attachments
+        """
+        if not self.has_attachments:
+            log.debug('Message has no attachments, skipping out early.')
             return False
 
-        response = (self.oauth, requests)[self.oauth is None].get(
-            self.att_url.format(
-                self.json['Id']), auth=self.auth, verify=self.verify, **kwargs)
-        log.info('response from O365 for retriving message attachments: %s',
-                 str(response))
-        json = response.json()
+        url = FluentMessage._get_url('attachments').format(
+            message_id=self.json['id'])
+        response = Connection.get_response(url, verify=self.verify, **kwargs)
 
-        for att in json['value']:
+        for attachment in response:
             try:
-                self.attachments.append(Attachment(att))
-                log.debug('successfully downloaded attachment for: %s.',
-                          self.auth[0])
-            except Exception as e:
-                log.info('failed to download attachment for: %s', self.auth[0])
+                self.attachments.append(Attachment(attachment))
+                log.debug('Successfully downloaded attachment {}'.format(
+                    attachment['name']))
+            except RuntimeError as _:
+                log.info('Failed downloading attachment {}'.format(
+                    attachment['name']))
 
         return len(self.attachments)
 
-    def sendMessage(self, user_id=None, **kwargs):
-        '''
-        Takes local variabls and forms them into a message to be sent.
+    def send(self, user_id=None, **kwargs):
+        """ Send the email
 
         :param user_id: User id (email) if sending as other user
-        '''
+        :return True or False (Success or Fail)
+        """
 
         headers = {'Content-Type': 'application/json', 'Accept': 'text/plain'}
 
@@ -141,53 +140,193 @@ class Message(object):
             return False
 
         if user_id:
-            url = self.send_as_url.format(user_id=user_id)
+            url = FluentMessage._get_url('send_as').format(user_id=user_id)
         else:
-            url = self.send_url
-        response = (self.oauth, requests)[self.oauth is None].post(
-            url, data, headers=headers, auth=self.auth, verify=self.verify,
-            **kwargs)
-        log.debug('response from server for sending message:' + str(response))
-        log.debug("respnse body: {}".format(response.text))
+            url = FluentMessage._get_url('send')
+
+        try:
+            response = Connection.get_response(url, method='POST', data=data,
+                                               headers=headers,
+                                               verify=self.verify,
+                                               **kwargs)
+        except RuntimeError as _:
+            return False
+
         if response.status_code != 202:
             return False
 
         return True
 
-    def markAsRead(self):
-        '''marks analogous message as read in the cloud.'''
-        read = '{"IsRead":true}'
+    def mark_as_read(self, **kwargs):
+        """ Marks this message as read in the cloud
+
+        :return True or False (Success or Fail)
+        """
+        data = '{"IsRead":true}'
         headers = {'Content-Type': 'application/json',
                    'Accept': 'application/json'}
+        url = FluentMessage._get_url('update').format(
+            message_id=self.json['id'])
         try:
-            response = (self.oauth, requests)[self.oauth is None].patch(
-                self.update_url.format(
-                    self.json['Id']), read, headers=headers, auth=self.auth,
-                verify=self.verify)
-        except:
+            response = Connection.get_response(url, method='PATCH', data=data,
+                                               headers=headers,
+                                               verify=self.verify,
+                                               **kwargs)
+        except RuntimeError as _:
             return False
         return response.ok
 
-    def moveToFolder(self, folder_id):
-        """
-        Move the message to a given folder
+    def move_to(self, folder_id, **kwargs):
+        """ Move the message to a given folder
 
         :param folder_id: Folder ID to move this message to
-        :returns: True on success
+        :return True or False (Success or Fail)
         """
-        move_url = 'https://outlook.office365.com/api/v1.0/me/messages/{0}/move'
         headers = {'Content-Type': 'application/json',
                    'Accept': 'application/json'}
-        post_data = {"DestinationId": folder_id}
-        try:
-            response = (self.oauth, requests)[self.oauth is None].post(
-                move_url.format(self.json['Id']),
-                json=post_data, headers=headers,
-                auth=self.auth, verify=self.verify)
-        except:
-            return False
+        data = {"DestinationId": folder_id}
 
+        url = FluentMessage._get_url('move').format(
+            message_id=self.json['id'])
+        try:
+            response = Connection.get_response(url, method='POST', data=data,
+                                               headers=headers,
+                                               verify=self.verify,
+                                               **kwargs)
+        except RuntimeError as _:
+            return False
         return response.ok
+
+    def _set_recipients(self, contacts, r_type='To'):
+        """ Set the `TO` recipients list.
+
+            Each recipient can be either of the below
+
+            dictionary: this must to be a dictionary formatted as such:
+                {"EmailAddress":{"Address":"recipient@example.com"}}
+                with other options such ass "Name" with address. but at minimum
+                it must have this.
+
+            or
+
+            simple email address
+
+            or
+
+            Contact object (Contact is part of this library)
+
+            or
+
+            Group object, which is a list of contacts
+            (Group is part of this library)
+
+        :param contacts: List of recipients
+        :param r_type: 'To' or 'Bcc' or 'Cc'
+        """
+        self.json[r_type + 'Recipients'] = []
+
+        if not isinstance(contacts, tuple):
+            contacts = [contacts]
+
+        log.debug('Setting recipients for {}: {}'.format(r_type, contacts))
+        for contact in contacts:
+            if isinstance(contact, Contact):
+                self.addRecipient(contact, r_type=r_type)
+            elif isinstance(contact, str):
+                if '@' in contact:
+                    self.addRecipient(contact, r_type=r_type)
+            elif isinstance(contact, dict):
+                self.json[r_type + 'Recipients'].append(contact)
+            elif isinstance(contact, Group):
+                for person in contact:
+                    self.addRecipient(person, r_type=r_type)
+            else:
+                raise RuntimeError(
+                    'Unknown contanct information {}'.format(contact))
+
+    def to(self, *recipients):
+        """ Set the `TO` recipients list.
+
+            Each recipient can be either of the below
+
+            dictionary: this must to be a dictionary formatted as such:
+                {"EmailAddress":{"Address":"recipient@example.com"}}
+                with other options such ass "Name" with address. but at minimum
+                it must have this.
+
+            or
+
+            simple email address
+
+            or
+
+            Contact object (Contact is part of this library)
+
+            or
+
+            Group object, which is a list of contacts
+            (Group is part of this library)
+
+        :param recipients: List of recipients
+        """
+        self._set_recipients(recipients, 'To')
+        return self
+
+    def cc(self, *recipients):
+        """ Set the `CC` recipients list.
+
+            Each recipient can be either of the below
+
+            dictionary: this must to be a dictionary formatted as such:
+                {"EmailAddress":{"Address":"recipient@example.com"}}
+                with other options such ass "Name" with address. but at minimum
+                it must have this.
+
+            or
+
+            simple email address
+
+            or
+
+            Contact object (Contact is part of this library)
+
+            or
+
+            Group object, which is a list of contacts
+            (Group is part of this library)
+
+        :param recipients: List of recipients
+        """
+        self._set_recipients(recipients, 'Cc')
+        return self
+
+    def bcc(self, *recipients):
+        """ Set the `BCC` recipients list.
+
+            Each recipient can be either of the below
+
+            dictionary: this must to be a dictionary formatted as such:
+                {"EmailAddress":{"Address":"recipient@example.com"}}
+                with other options such ass "Name" with address. but at minimum
+                it must have this.
+
+            or
+
+            simple email address
+
+            or
+
+            Contact object (Contact is part of this library)
+
+            or
+
+            Group object, which is a list of contacts
+            (Group is part of this library)
+
+        :param recipients: List of recipients
+        """
+        self._set_recipients(recipients, 'Bcc')
+        return self
 
     def getSender(self):
         '''get all available information for the sender of the email.'''
@@ -323,21 +462,54 @@ class Message(object):
             except:
                 self.json['Body'] = {}
 
-    def setCategory(self, category_name, **kwargs):
-        "Sets the email's category"
-        self.update_category(self, category_name, **kwargs)
+    def category(self, category_name, **kwargs):
+        """ Set the category of the message
 
-    def update_category(self, category_name, **kwargs):
-        category = '{{"Categories":["{}"]}}'.format(category_name)
+        :param category_name: category to add to the message
+        :return:
+        """
+        data = '{{"Categories":["{}"]}}'.format(category_name)
         headers = {'Content-Type': 'application/json',
                    'Accept': 'application/json'}
+        url = FluentMessage._get_url('update').format(
+            message_id=self.json['id'])
         try:
-            response = (self.oauth, requests)[self.oauth is None].patch(
-                self.update_url.format(
-                    self.json['Id']), category, headers=headers, auth=self.auth,
-                verify=self.verify, **kwargs)
-        except:
+            response = Connection.get_response(url, method='PATCH', data=data,
+                                               headers=headers,
+                                               verify=self.verify,
+                                               **kwargs)
+        except RuntimeError as _:
             return False
         return response.ok
 
-# To the King!
+    @deprecated(category)
+    def setCategory(self, category_name, **kwargs):
+        self.set_category(category_name, **kwargs)
+
+    @deprecated(category)
+    def update_category(self, category_name, **kwargs):
+        self.set_category(category_name, **kwargs)
+
+    @deprecated(fetch_attachments)
+    def fetchAttachments(self, **kwargs):
+        return self.fetch_attachments(**kwargs)
+
+    @deprecated(send)
+    def sendMessage(self, user_id=None, **kwargs):
+        return self.send(user_id, **kwargs)
+
+    @deprecated(mark_as_read)
+    def markAsRead(self, **kwargs):
+        return self.mark_as_read(**kwargs)
+
+    @deprecated(move_to)
+    def moveToFolder(self, folder_id, **kwargs):
+        return self.move_to(folder_id, **kwargs)
+
+    @deprecated(to, cc, bcc)
+    def setRecipients(self, val, r_type='To'):
+        return self._set_recipients(val, r_type)
+
+
+# Below step added for backward compatibility
+Message = FluentMessage
