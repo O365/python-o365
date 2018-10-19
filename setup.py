8f502e8b30b004c6f21c7888ecc03bd3b8b681f7
Jandro <janscas@users.noreply.github.com>
2018-10-19 15:13:02 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-long_desc = """
-This project aims is to make it easy to interact with Microsoft Graph and Office 365 Email, Contacts, Calendar, OneDrive, etc.
-
-This project is inspired on the super work done by [Toben Archer](https://github.com/Narcolapser) [Python-O365](https://github.com/Narcolapser/python-o365).
-The oauth part is based on the work done by [Royce Melborn](https://github.com/roycem90) which is now integrated with the original project.
-
-I just want to make this project different in almost every sense, and make it also more pythonic.
-So I ended up rewriting the hole project from scratch.
-
-The result is a package that provides a lot of the Microsoft Graph and Office 365 API capabilities.
-
-This is for example how you send a message:
-
-from pyo365 import Account
-
-credentials = ('client_id', 'client_secret')
-
-account = Account(credentials, auth_method='oauth')
-m = account.new_message()
-m.to.add('to_example@example.com')
-m.subject = 'Testing!'
-m.body = "George Best quote: I've stopped drinking, but only while I'm asleep."
-m.send()
-"""
 
 # Available classifiers: https://pypi.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
