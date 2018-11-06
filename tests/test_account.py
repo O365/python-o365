2f46e9f08aa63bc0b82bf3321081d2ea4046aceb
Toben Archer <Toben.Archer@usd.edu>
2018-11-06 09:23:52 -0600

    This should get tests working.
    I believe the reason why my tests were failing previously is
    because if no tests are run, pytest returns a none zero result. So
    then Travis CI see it as a failure. Now that I've got some basic
    tests in place, that problem should be solved.

---
+++ tests/test_account.py
@@ -0,0 +1,16 @@
+from O365 import Account
+from O365 import Message
+
+
+class TestAccount:
+
+    def setup_class(self):
+        credentials = ("client id","client secret")
+        self.account = Account(credentials)
+
+    def teardown_class(self):
+        pass
+    
+    def test_get_message(self):
+        message = self.account.new_message()
+        assert isinstance(message,Message)
