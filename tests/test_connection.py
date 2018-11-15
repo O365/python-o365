3dd984fb62f01b9467d691f670ee3725e02bc626
Toben Archer <Toben.Archer@usd.edu>
2018-11-15 10:42:12 -0600

    Protocols covered.
    I've also added to the release script to include a convenience
    method for running tests. It includes levels of verbosity and the
    option to produce annotated code files to let you know where the
    coverage is lacking.
    
    I've decided to make seperate test files for each class and
    depending on the circumstances it's decendants. Some classes will
    have have decendants bigger than themselves so those decendants
    will go into their own test. But protocol's decendants were pretty
    small so I just included them in protocol's tests. What ever makes
    sense is basically what I'm going for.

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,24 +1,8 @@
-from O365.connection import Connection, Protocol, MSGraphProtocol, MSOffice365Protocol
 import pytest
 import json
 
-class TestProtocol:
+from O365.connection import Connection, Protocol, MSGraphProtocol, MSOffice365Protocol, DEFAULT_SCOPES
 
-    def setup_class(self):
-        self.proto = Protocol(protocol_url="testing", api_version="0.0")
-
-    def teardown_class(self):
-        pass
-
-    def test_blank_protocol(self):
-        with pytest.raises(ValueError):
-            p = Protocol()
-
-    def test_to_api_case(self):
-        assert(self.proto.to_api_case("CaseTest") == "case_test")
-
-    def test_get_iana_tz(self):
-        assert(self.proto.get_iana_tz('Greenwich Standard Time') == 'Atlantic/St_Helena')
 
 class TestConnection:
 
