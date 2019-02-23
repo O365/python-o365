933802bf708aff784177a06437c9d531fb3a134b
Robert Habermann <mail@rhab.de>
2019-02-23 19:28:27 +0100

    fix minor typos and PEP8 issues

--- examples/calendarCookbook.py
+++ examples/calendarCookbook.py
@@ -8,10 +8,12 @@ p = 'password'
 schedule = Schedule((e, p))
 result = schedule.getCalendars()
 
-# Funciton to invoke getName() and bind to var result
+
+# Function to invoke getName() and bind to var result
 def cal_name():
-    result = cal.getName() # This will get the name of a calendar
+    result = cal.getName()  # This will get the name of a calendar
     return result
+
 
 # Show name of each calendar
 print('\nHere are your calendars:\n')
@@ -22,8 +24,8 @@ for cal in schedule.calendars:
 print('\nHere are all your upcoming events:\n')
 for cal in schedule.calendars:
     result = cal_name()
-    events = cal.getEvents() # This will create an Event object
+    events = cal.getEvents()  # This will create an Event object
     for x in cal.events:
-        contents = x.getSubject() # This will get the subject
-        date = x.getStart() # This will get the start time and we'll parse it below
+        contents = x.getSubject()  # This will get the subject
+        date = x.getStart()  # This will get the start time and we'll parse it below
         print('{}-{} | {} : {}'.format(date.tm_mon, date.tm_mday, result, contents))
