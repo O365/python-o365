760f1aca8115038e6e7ec34536d2e83d2502b159
Andrew Berezovskyi <andriib@kth.se>
2020-12-20 22:14:57 +0100

    Add Task.importance and Task.is_starred properties

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '2.0.13'
+VERSION = '2.0.14'
 
 # Available classifiers: https://pypi.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
