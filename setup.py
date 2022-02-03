3bb1333ac0c9632dc28506c602b6b51b015dbbe4
Alejandro Casanovas <janscas@users.noreply.github.com>
2022-02-03 14:38:22 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '2.0.18'
+VERSION = '2.0.18.1'
 
 # Available classifiers: https://pypi.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
@@ -34,7 +34,7 @@ requires = [
     'requests_oauthlib>=1.2.0',
     'python-dateutil>=2.7',
     'pytz>=2018.5',
-    'tzlocal>=1.5.0,<3.*',
+    'tzlocal>=4.0',
     'beautifulsoup4>=4.0.0',
     'stringcase>=1.2.0'
 ]
