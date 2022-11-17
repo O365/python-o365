194c0fcde66fda635b7b47081f658b2bbcce6054
Alejandro Casanovas <janscas@users.noreply.github.com>
2022-11-17 12:28:55 +0100

    added python 3.11

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
     'Operating System :: OS Independent',
 ]
 
