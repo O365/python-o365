2f85c56e049301775b14498ad710a999f0cdd6ec
Alejandro Casanovas <alejcas@users.noreply.github.com>
2025-01-21 17:17:40 +0100

    fixed conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-print(f"PATH ES: {os.path.abspath('../../O365')}")
 
 
 # -- Project information -----------------------------------------------------
