800d6ef0de6d5e2556c060ac5bba0dfbe6390637
Alejandro Casanovas <alejcas@users.noreply.github.com>
2025-08-04 15:35:22 +0200

    bumb version to release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '2.1.4'
+VERSION = '2.1.5'
 
 # Available classifiers: https://pypi.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
