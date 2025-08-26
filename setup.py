067146e82c87aa2476d0d5150e44cdddc17a1215
RogerSelwyn <rogers@selwyn.org.uk>
2025-08-26 11:27:38 +0100

    Remove support for Python 3.9

--- setup.py
+++ setup.py
@@ -7,19 +7,18 @@ VERSION = '2.1.5'
 
 # Available classifiers: https://pypi.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: Apache Software License',
-    'Topic :: Office/Business :: Office Suites',
-    'Topic :: Software Development :: Libraries',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
-    'Programming Language :: Python :: 3.12',
-    'Programming Language :: Python :: 3.13',
-    'Operating System :: OS Independent',
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Topic :: Office/Business :: Office Suites",
+    "Topic :: Software Development :: Libraries",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Operating System :: OS Independent",
 ]
 
 
@@ -38,20 +37,20 @@ requires = [
 ]
 
 setup(
-    name='o365',
+    name="o365",
     version=VERSION,
     packages=find_packages(),
-    url='https://github.com/O365/python-o365',
-    license='Apache License 2.0',
-    author='Alejcas, Roycem90, Narcolapser',
-    author_email='alejcas@users.noreply.github.com',
-    maintainer='alejcas',
-    maintainer_email='alejcas@users.noreply.github.com',
-    description='Microsoft Graph API made easy',
-    long_description=read('README.md'),
+    url="https://github.com/O365/python-o365",
+    license="Apache License 2.0",
+    author="Alejcas, Roycem90, Narcolapser",
+    author_email="alejcas@users.noreply.github.com",
+    maintainer="alejcas",
+    maintainer_email="alejcas@users.noreply.github.com",
+    description="Microsoft Graph API made easy",
+    long_description=read("README.md"),
     long_description_content_type="text/markdown",
     classifiers=CLASSIFIERS,
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=requires,
     setup_requires=["wheel"],
 )
