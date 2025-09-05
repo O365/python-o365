026b120b826ce20fb5924f52adecf21a265b1205
Alejandro Casanovas <alejcas@users.noreply.github.com>
2025-09-05 14:10:44 +0200

    Old Query object removed. Now we default to the new QueryBuilder object Deprecation message removed

--- O365/utils/__init__.py
+++ O365/utils/__init__.py
@@ -3,11 +3,10 @@ from .utils import ApiComponent, OutlookWellKnowFolderNames
 from .utils import CaseEnum, ImportanceLevel, TrackerSet
 from .utils import Recipient, Recipients, HandleRecipientsMixin
 from .utils import NEXT_LINK_KEYWORD, ME_RESOURCE, USERS_RESOURCE
-from .utils import OneDriveWellKnowFolderNames, Pagination, Query
+from .utils import OneDriveWellKnowFolderNames, Pagination
 from .token import BaseTokenBackend, FileSystemTokenBackend, FirestoreBackend, AWSS3Backend, AWSSecretsBackend, EnvTokenBackend, BitwardenSecretsManagerBackend, DjangoTokenBackend
 from .range import col_index_to_label
 from .windows_tz import get_iana_tz, get_windows_tz
 from .consent import consent_input_token
 from .casing import to_snake_case, to_pascal_case, to_camel_case
-
-from .query import QueryBuilder as ExperimentalQuery, CompositeFilter
+from .query import QueryBuilder, CompositeFilter
